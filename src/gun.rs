00883f9f106591cc55a72ffda0dc817d9af81594
Evgeniy Terekhin <evgeniy@tvbeat.com>
2022-11-26 21:34:07 +0200

    guns.v1.ron

---
+++ src/gun.rs
@@ -0,0 +1,41 @@
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub enum Tier {
+    S,
+    A,
+    B,
+    C,
+    D,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub enum Kind {
+    Automatic,
+    Beam,
+    Burst,
+    Charged,
+    Semiautomatic,
+    Type,
+    Varies,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct V1 {
+    name: String,
+    notes: Option<String>,
+    quote: Option<String>,
+    tier: Option<Tier>,
+    #[serde(rename = "type")]
+    kind: Kind,
+    dps: Option<String>,
+    magazine_size: Option<String>,
+    ammo_capacity: Option<String>,
+    damage: Option<String>,
+    fire_rate: Option<String>,
+    reload_time: Option<String>,
+    shot_speed: Option<String>,
+    range: Option<String>,
+    force: Option<String>,
+    spread: Option<String>,
+}
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+mod gun;
+
+use std::fs::File;
+use std::io::Write;
+
+fn main() {
+    let input = std::env::args().nth(1).unwrap();
+    let input = File::open(input).unwrap();
+
+    let output = std::env::args().nth(2).unwrap();
+    let mut output = File::create(output).unwrap();
+
+    let mut csv_reader = csv::ReaderBuilder::new()
+        .delimiter(b'|')
+        .has_headers(true)
+        .from_reader(input);
+
+    let records = csv_reader
+        .deserialize()
+        .collect::<Result<Vec<gun::V1>, _>>()
+        .unwrap();
+    let records = ron::ser::to_string_pretty(&records, Default::default()).unwrap();
+
+    writeln!(&mut output, "{}", records).unwrap();
+}
